35a54c820bf154ccd4ee73c79fcbdf84348bfec0
ZaccariaEzzarouali <zaccaria.ezza@gmail.com>
2022-10-06 13:29:45 +0200

    first commit

---
+++ TournamentTracker/TrackerLibrary/TeamModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    internal class TeamModel
+    {
+        public List<Person> TeamMember { get; set; } = new List<Person>();
+        public String TeamName { get; set; }
+
+    }
+}
